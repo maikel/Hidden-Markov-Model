67a53ffa54f6e555d43578e54cd49a6b86256376
Maikel Nadolski <maikel.nadolski@fu-berlin.de>
2016-03-12 10:49:35 +0100

    add binary stream

--- forward.cpp
+++ forward.cpp
@@ -24,19 +24,40 @@ enum Exit_Error_Codes {
 };
 
 template <class float_type, class index_type>
-  void accumulate_and_view_transform(
+  void accumulate_scaling_and_write_alpha_to_file(
       std::vector<index_type> const& sequence,
       maikel::hmm::hidden_markov_model<float_type> const& model)
   {
+    std::size_t states = model.states();
+//    std::vector<Eigen::RowVectorXd> alphas(T, Eigen::RowVectorXd(states));
+    std::ofstream alphas("alphas.bin", std::ofstream::binary);
     BOOST_LOG_TRIVIAL(info) << "Starting forward algorithm with storing scaling factors into std::vector.";
     BOOST_LOG_TRIVIAL(info) << "Use accumulate on a view::transformed scaling list.";
-    MAIKEL_PROFILER;
-
     float_type logprob = 0.0;
-    for (auto&& scaled_alpha : maikel::hmm::forward(sequence, model))
-      logprob += std::log(scaled_alpha.first);
+    std::size_t datalen = sizeof(float_type)*states;
+    { MAIKEL_PROFILER;
+      std::ostreambuf_iterator<char> out(alphas);
+      for (auto&& scaled_alpha : maikel::hmm::forward(sequence, model)) {
+        logprob += std::log(scaled_alpha.first);
+        std::copy_n(reinterpret_cast<const char*>(scaled_alpha.second.data()), datalen, out);
+      }
+    }
     std::cout << -logprob << std::endl;
   }
+
+template <class T>
+void read_alphas_from_bin(const maikel::hmm::hidden_markov_model<T>& hmm)
+{
+  MAIKEL_PROFILER;
+  std::ifstream alphas("alphas.bin", std::ifstream::binary);
+  Eigen::Matrix<T, 1, Eigen::Dynamic> alpha(hmm.states());
+  std::size_t data_len = sizeof(T)*alpha.size();
+  std::istreambuf_iterator<char> in(alphas), end;
+  while (in != end) {
+    std::copy_n(in, data_len, reinterpret_cast<char*>(alpha.data()));
+    std::advance(in, data_len+1);
+  }
+}
 
 int main(int argc, char *argv[])
 {
@@ -52,7 +73,7 @@ int main(int argc, char *argv[])
   std::ifstream model_input(argv[1]);
   auto model = maikel::hmm::read_hidden_markov_model<float_type>(model_input);
 
-  std::vector<int> symbols { 0, 1 };
+  std::vector<int> symbols { 0,1 };
   std::map<int,index_type> symbol_to_index = maikel::map_from_symbols<index_type>(symbols);
   std::ifstream sequence_input(argv[2]);
   BOOST_LOG_TRIVIAL(info) << "Reading sequence ...";
@@ -62,7 +83,8 @@ int main(int argc, char *argv[])
   maikel::function_profiler::print_statistics(std::cerr);
 
   maikel::function_profiler::reset();
-  accumulate_and_view_transform(sequence, model);
+  accumulate_scaling_and_write_alpha_to_file(sequence, model);
+  read_alphas_from_bin(model);
   maikel::function_profiler::print_statistics(std::cerr);
 
   return exit_success;
