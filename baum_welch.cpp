db97cd0344a3cb55afdd2d341fd2c5c326f6476a
Maikel Nadolski <maikel.nadolski@gmail.com>
2016-03-23 00:20:57 +0100

    valgrind baum_welch

--- baum_welch.cpp
+++ baum_welch.cpp
@@ -25,6 +25,49 @@ using namespace std;
 using namespace maikel;
 using row_vector = hmm::hidden_markov_model<double>::row_vector;
 
+double calculate_alpha(vector<uint8_t> const& sequence, hmm::hidden_markov_model<double> const& hmm,
+    vector<double>& scaling, vector<row_vector>& alphas)
+{
+  double logprob = 0;
+  size_t count = 0;
+  for (pair<double, row_vector> const& sal
+      : hmm::forward(begin(sequence), end(sequence), hmm)) {
+    scaling[count] = sal.first;
+    alphas[count]  = sal.second;
+    logprob += log(sal.first);
+    ++count;
+  }
+  return logprob;
+}
+
+void calculate_beta(vector<uint8_t> const& sequence, hmm::hidden_markov_model<double> const& hmm,
+    vector<double> const& scaling, vector<row_vector>& betas)
+{
+  size_t count = 0;
+  for (row_vector const& beta
+      : hmm::backward(sequence.rbegin(), sequence.rend(), scaling.rbegin(), hmm)) {
+    betas[sequence.size()-count-1] = beta;
+    ++count;
+  }
+}
+
+template <class Update>
+void update_hmm(
+    Update& update,
+    vector<uint8_t>& sequence,
+    vector<row_vector>& alphas,
+    vector<row_vector>& betas,
+    vector<double>& scaling,
+    row_vector& pi,
+    hmm::hidden_markov_model<double>& hmm)
+{
+  auto matrices = update(begin(sequence), end(sequence), begin(alphas), begin(betas), scaling.back(), hmm);
+  pi = alphas[0].cwiseProduct(betas[0]) / scaling[0];
+  //    cout << "step #" << step << " log P(O|lambda): " << -logprob << "\n";
+  //    cout << matrices.first.format(Eigen::IOFormat(5)) << endl;
+  hmm = hmm::hidden_markov_model<double>(matrices.first, matrices.second, pi);
+}
+
 int main(int argc, char** argv)
 {
   if (argc < 3) {
@@ -55,29 +98,12 @@ int main(int argc, char** argv)
     ++step;
     // calculate alphas
     swap(logprob_old, logprob);
-    logprob = 0;
-    size_t count = 0;
-    for (pair<double, row_vector> const& sal
-        : hmm::forward(begin(sequence), end(sequence), hmm)) {
-      scaling[count] = sal.first;
-      alphas[count]  = sal.second;
-      logprob += log(sal.first);
-      ++count;
-    }
+    logprob = calculate_alpha(sequence, hmm, scaling, alphas);
 
     // calculate betas
-    count = 0;
-    for (row_vector const& beta
-        : hmm::backward(sequence.rbegin(), sequence.rend(), scaling.rbegin(), hmm)) {
-      betas[sequence.size()-count-1] = beta;
-      ++count;
-    }
+    calculate_beta(sequence, hmm, scaling, betas);
 
-    auto matrices = update(begin(sequence), end(sequence), begin(alphas), begin(betas), scaling.back(), hmm);
-    pi = alphas[0].cwiseProduct(betas[0]) / scaling[0];
-    cout << "step #" << step << " log P(O|lambda): " << -logprob << "\n";
-    cout << matrices.first.format(Eigen::IOFormat(5)) << endl;
-    hmm = hmm::hidden_markov_model<double>(matrices.first, matrices.second, pi);
+    update_hmm(update, sequence, alphas, betas, scaling, pi, hmm);
   } while (!almost_equal<double,100>(logprob, logprob_old));
 
   cout << "steps: " << step << ", A:\n" << hmm.transition_matrix() << endl;
